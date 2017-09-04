afe3c6b12f423495274573dc69807aa7fe03fe4c
Billy Jacoby <billyjacoby@nike.local>
2017-09-04 17:03:34 -0400

    added comments everywhere to make things more clear for

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,15 @@
+// This is the client side code
+
+// the socket variable is declared in order to make the library accessible
 var socket = io();
 
+// a necessary custom implementation of an autosroll feature
 function scrollToBottom () {
   // Selectors
   var messages = jQuery("#messages");
   var newMessage = messages.children('li:last-child');
   // Heights
+  // the following three variables are accessible via jquery using props? i dont 100% understand this
   var clientHeight = messages.prop('clientHeight');
   var scrollTop = messages.prop('scrollTop');
   var scrollHeight = messages.prop('scrollHeight');
@@ -12,31 +17,41 @@ function scrollToBottom () {
   var lastMessageHeight = newMessage.prev().innerHeight();
 
   if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    // the following sets the scrollTop variable, making the screen scroll
     messages.scrollTop(scrollHeight);
   }
 };
 
+// this is actually an unnecessary funcion which simply prints a message to the client side console when connected.
 socket.on('connect', function()  {
   console.log('Connected to server');
 
 });
 
+// this too, except when disconnected
 socket.on('disconnect', function() {
   console.log('Disconnected from server');
 });
 
+// when a 'newMessage' event is emitted from the server the following code fires
 socket.on('newMessage', function(message) {
+  // formatted time using moment library on the client side
   var formattedTime = moment(message.createdAt).format('h:mm a')
+  // select just the HTML from the mustache template in index.html
   var template = jQuery('#message-template').html();
+  // this is how the data is passed into the mustache template, via the render function
   var html = Mustache.render(template, {
     text: message.text,
     from: message.from,
     createdAt: formattedTime
   });
+  // jquery method for appending the newly recieved message
   jQuery("#messages").append(html);
+  // implement custom autoscroll function
   scrollToBottom();
 });
 
+// does the same as above, except for the specific case of a location message
 socket.on('newLocationMessage', function(message) {
   var formattedTime = moment(message.createdAt).format('h:mm a')
   var template = jQuery("#location-message-template").html();
@@ -49,11 +64,17 @@ socket.on('newLocationMessage', function(message) {
   scrollToBottom();
 });
 
+
+// using jquery to manipulate what happens when the submit button is pressed
 jQuery('#message-form').on('submit', function (e) {
+  // this prevents default action of submit button, which would refresh the page
   e.preventDefault();
 
+  // set variable equal to the message text field
   var messageTextbox = jQuery('[name=message]')
 
+  // this is where the client actually emits a 'createMessage' event to the server
+  // the event type or name or whatever is the first argument, and the data to be passed is the second argument
   socket.emit('createMessage', {
     from: 'User',
     text: messageTextbox.val()
@@ -62,21 +83,27 @@ jQuery('#message-form').on('submit', function (e) {
   });
 });
 
+// bascially does the same as above except for a regular button, without a form field
 var locationButton = jQuery('#send-location');
+// the button is not a submit button, so on 'click' must be used
 locationButton.on('click', function () {
+  // checks for location support
   if (!navigator.geolocation) {
     return alert('Geolocation is not supported by your browser.');
   }
 
+  // while the action is working the button is temporarily disabled
   locationButton.attr('disabled', 'disabled').text('Sending Location...');
 
   navigator.geolocation.getCurrentPosition(function (position) {
+    // set the send location button back to normal and enable it again; the call to API is finished
     locationButton.removeAttr('disabled').text('Send Location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    // this is the callback function passed back to the server to be called once the first action is attempted
     locationButton.removeAttr('disabled').text('Send Location');
     alert('Unable to fetch location.');
   })
--- server/server.js
+++ server/server.js
@@ -1,42 +1,61 @@
+// library for joining paths neatly
 const path = require('path');
+// node standard library for http handling
 const http = require('http');
+// library for creating a server and routes
 const express = require('express');
+// library for two way communcation between the clietn and the server
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+// creates the public path for files to be accessible to the client
 const publicPath = path.join(__dirname, '../public' );
+// sets the port to either 3000 or whatever Heroku needs it to be in order for it to work
 const port = process.env.PORT || 3000;
+// initialize express application
 var app = express();
+// create variable for accessing node http server
 var server = http.createServer(app);
+// create a variable for accessing the socket main function on the node http server
 var io = socketIO(server);
 
+// allow the client to view and server all files in the /public directory
 app.use(express.static(publicPath));
 
+// initlaizes the socket connection. the funciton takes socket as a variable
 io.on('connection', (socket) => {
+  // this logs new user connected to the server console upon a new connection
   console.log('New user connected');
 
-  // from Admin, to new user
+  // emit a 'newMessage' event from Admin, to new user
   socket.emit('newMessage', generateMessage("Admin", "Welcome to the chat app"));
 
-  // from admin, to everyone except the new user
+  // emit a 'createMessage' event from admin, to everyone except the new user
   socket.broadcast.emit('newMessage', generateMessage("Admin", "New user has joined"));
 
+  // whenever a 'createMessage' event is emitted from a client the following code fires
   socket.on('createMessage', (message, callback) => {
+    // logs this message to the server console
     console.log('createMessage', message);
+    // io.emit will perform the function passed into it to every single user, including the one who sent it
     io.emit('newMessage', generateMessage(message.from, message.text));
+    // im not 100% sure what this does, something about authoriation or something?
     callback();
   });
 
+  // whwnever a 'createLocationMessage' event is emitted from a client this code fires
   socket.on('createLocationMessage', (coords) => {
+    // again, performs this function for everyone on the server
     io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
+    // simply prints a statemtnet to the server console when a user disconnects
     console.log('User was disconnected');
   });
 });
 
-
+// server.listen must be used rather than app.listen when using socket.io 
 server.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });

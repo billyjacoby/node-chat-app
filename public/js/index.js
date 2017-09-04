3cc9e392f6cb6c99525ff8a71541f42af5a693eb
Billy Jacoby <billyjacoby@nike.local>
2017-09-03 21:40:39 -0400

    added css for chat app

--- public/js/index.js
+++ public/js/index.js
@@ -31,11 +31,13 @@ socket.on('newLocationMessage', function(message) {
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]')
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val('');
   });
 });
 
@@ -45,14 +47,16 @@ locationButton.on('click', function () {
     return alert('Geolocation is not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending Location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
-    console.log(position);
-    console.log(position.coords.latitude);
+    locationButton.removeAttr('disabled').text('Send Location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
-    alert('Unable to fetch location.')
+    locationButton.removeAttr('disabled').text('Send Location');
+    alert('Unable to fetch location.');
   })
 });
